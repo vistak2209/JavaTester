5a5120ecbea539fa5dd1cce81be1fa89a8e65ef6
denny.du <denny.du@ecareme.com>
2022-07-27 13:01:27 +0800

    Add NullHandler

---
+++ src/main/java/org/example/design/proxy/nullhander/Position.java
@@ -0,0 +1,44 @@
+package org.example.design.proxy.nullhander;
+
+public class Position {
+    private String title;
+    private Person person;
+    public Position(String jobTitle, Person employee){
+        this.title = jobTitle;
+        this.person = employee;
+        if(person==null){
+            this.person = Person.NULL;
+        }
+    }
+    public Position(String jobTitle){
+        this.title = jobTitle;
+        this.person = Person.NULL;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+        if(this.person==null){
+            person = Person.NULL;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Postition{" +
+                "title='" + title + '\'' +
+                ", person=" + person +
+                '}';
+    }
+}
---
+++ src/main/java/org/example/design/proxy/nullhander/Staff.java
@@ -0,0 +1,46 @@
+package org.example.design.proxy.nullhander;
+
+import java.util.ArrayList;
+
+public class Staff extends ArrayList<Position> {
+    public void add(String title,Person person){
+        add(new Position(title,person));
+    }
+    public void add(String... titles){
+        for(String title:titles){
+            add(new Position(title));
+        }
+    }
+    public Staff(String... titles){
+        add(titles);
+    }
+    public boolean positionAvailable(String title){
+        for(Position position: this){
+            if(position.getTitle().equals(title)&&position.getPerson()==Person.NULL){
+                return true;
+            }
+        }
+        return false;
+    }
+    public void fillPosition(String title,Person hire){
+        for(Position position:this){
+            if(position.getTitle().equals(title)&&position.getPerson()==Person.NULL){
+                position.setPerson(hire);
+                return;
+            }
+        }
+        throw new RuntimeException("Position "+title+" not available");
+    }
+    public static void main(String[] args){
+        Staff staff = new Staff("AAA","BBB","CCC","DDD","EEE","FFF");
+        staff.fillPosition("AAA",new Person("Me","Last","The Top,Lonly At"));
+        staff.fillPosition("CCC",new Person("Jenet","Planner","Taiwan"));
+        if(staff.positionAvailable("EEE")){
+            staff.fillPosition("EEE",new Person("Bob","Coder","Canada"));
+        }
+        if(staff.positionAvailable("AD")){
+            staff.fillPosition("AD",new Person("Bob","Coder","Canada"));
+        }
+        System.out.println(staff);
+    }
+}
