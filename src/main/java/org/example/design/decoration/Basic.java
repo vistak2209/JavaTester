fb8924400a70c10669b8184ffa35ca721e1519f9
denny.du <denny.du@ecareme.com>
2022-08-10 08:25:54 +0800

    Decorator mode

---
+++ src/main/java/org/example/design/decoration/Basic.java
@@ -0,0 +1,11 @@
+package org.example.design.decoration;
+
+public class Basic {
+    private String value;
+    public String get() {
+        return value;
+    }
+    public void set(String value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/org/example/design/decoration/DynamicProxyMixin.java
@@ -0,0 +1,12 @@
+package org.example.design.decoration;
+
+import static org.example.design.generator.base.tuple.Tuple.tuple;
+
+public class DynamicProxyMixin {
+    public static void main(String[] args){
+        Object mixin = MixinProxy.newInstance(
+                //tuple(new BasicImp()
+
+        );
+    }
+}
